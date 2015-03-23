44373d540a71f2bf95e545a66aaaf0f7d290bd7a
iaindonald <iain.s.donald@gmail.com>
2015-03-23 09:52:08 +0000

    New upload example
    New example for uploading template files between HotDocs Developer and a
    host application.

---
+++ HotDocs Open SDK Examples/SdkExample15/Models/Home/PackageUploadViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+
+namespace SdkExample9.Models
+{
+    public class PackageUploadViewModel
+    {
+        public HttpPostedFileBase HD_Upload0 { get; set; }
+        public HttpPostedFileBase HD_Package_Manifest { get; set; }        
+        public PublishTemplateMetaDataModel HD_Template { get; set; }
+
+        public int? TotalTemplatesCount { get; set; }
+        public int? CurrentTemplateIndex { get; set; }
+    }
+
+    public class PublishTemplateMetaDataModel
+    {        
+        public string Title0 { get; set; }   
+        public string Description0 { get; set; }
+        public string LibraryPath0 { get; set; }
+        public DateTime? Expiration_Date0 { get; set; }
+        public int Warning_Days0 { get; set; }
+        public int Extension_Days0 { get; set; }
+        public string CommandLineSwitches0 { get; set; }
+        public string UploadItemType0 { get; set; }
+        public string Filename0 { get; set; }        
+    }
+}
