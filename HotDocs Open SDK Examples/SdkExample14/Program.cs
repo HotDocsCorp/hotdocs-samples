d1fa91dc9fcda96b064888c2afe48e0e45635956
iaindonald <iain.s.donald@gmail.com>
2015-05-07 10:14:17 +0100

    Changes to HMAC calculation in Example 14
    Added additional HMAC calculation code.

--- HotDocs Open SDK Examples/SdkExample14/Program.cs
+++ HotDocs Open SDK Examples/SdkExample14/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using HotDocs.Sdk.Server.Contracts;
 using System.IO;
+using System.Collections.Generic;
+using System.Text;
 
 namespace SdkExample14
 {
@@ -10,8 +12,8 @@ namespace SdkExample14
     class Program
     {
         static void Main(string[] args)
-        {            
-            var subscriberId = "example-subscriber-id";
+        {
+            var subscriberId = "example-subscribed-id";
             var signingKey = "example-signing-key";
             var timestamp = DateTime.UtcNow;
             var packageId = "HelloWorld";
@@ -33,18 +35,7 @@ namespace SdkExample14
         private static string GetHMAC(string signingKey, DateTime timestamp, string subscriberId, string packageId)
         {
             // Calculate the HMAC
-            var hmac = HMAC.CalculateHMAC(signingKey, timestamp, subscriberId, packageId, null, true, "");
-
-            // Validate the HMAC
-            try
-            {
-                HMAC.ValidateHMAC(hmac, signingKey, timestamp, subscriberId, packageId, null, true, "");
-            }
-            catch (HMACException hmacException)
-            {
-                throw hmacException;
-            }
-
+            var hmac = CalculateHMAC(signingKey, timestamp, subscriberId, packageId, null, true, "");
             return hmac;
         }
         
@@ -80,6 +71,53 @@ namespace SdkExample14
             };
             
             return fileContent;
+        }
+
+        public static string CalculateHMAC(string signingKey, params object[] paramList)
+        {
+            byte[] key = Encoding.UTF8.GetBytes(signingKey);
+            string stringToSign = Canonicalize(paramList);
+            byte[] bytesToSign = Encoding.UTF8.GetBytes(stringToSign);
+            byte[] signature;
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA1(key))
+            {
+                signature = hmac.ComputeHash(bytesToSign);
+            }
+
+            return Convert.ToBase64String(signature);
+        }
+
+        public static string Canonicalize(params object[] paramList)
+        {
+            if (paramList == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var strings = paramList.Select(param =>
+            {
+                if (param is string || param is int || param is Enum || param is bool)
+                {
+                    return param.ToString();
+                }
+                
+                if (param is DateTime)
+                {
+                    DateTime utcTime = ((DateTime)param).ToUniversalTime();
+                    return utcTime.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                }
+                
+                if (param is Dictionary<string, string>)
+                {
+                    var sorted = ((Dictionary<string, string>)param).OrderBy(kv => kv.Key);
+                    var stringified = sorted.Select(kv => kv.Key + "=" + kv.Value).ToArray();
+                    return string.Join("\n", stringified);
+                }
+                return "";
+            });
+
+            return string.Join("\n", strings.ToArray());
         }
     }
 }
